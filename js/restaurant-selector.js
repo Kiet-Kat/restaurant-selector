f08627dc9a11da2ce3598660c17889be28781a74
Kiet Huynh <Kiet.Huynh@kimley-horn.com>
2019-07-10 12:23:15 -0400

    Adjusted colors and added eat out button

--- js/restaurant-selector.js
+++ js/restaurant-selector.js
@@ -11,25 +11,37 @@ var restaurants = [
     "Cowbell",
     "Mr. Tokyo",
     "Taco Bell",
-    "Soho Bistro",
-    "Eat at Home"
+    "Soho Bistro"
 ]
+
+function getRestaurant(){
+    return restaurants[Math.floor(Math.random() * restaurants.length)];
+}
 
 $(document).ready(function(){
     $('.roll1').click(function(){
-        var result = restaurants[Math.floor(Math.random() * restaurants.length)];
+        var result = getRestaurant();
         $('#result').text(result);
     });
     
     $('.roll3').click(function() {
         var result = [];
         while (result.length < 3){
-            restaurant = restaurants[Math.floor(Math.random() * restaurants.length)];
+            restaurant = getRestaurant();
             if (result.includes(restaurant)){
             }else{
                 result.push(restaurant);
             }
         }
-        $('#result').text(result[0] + " , " + result[1] + " , " +  result[2]);
+        $('#result').text(result[0] + ", " + result[1] + ", " +  result[2]);
+    });
+
+    $(".eat-out").click(function(){
+        var eatOut = Math.random() >= 0.65;
+        if (eatOut == true){
+            $('#result').text("Eat out at " + getRestaurant());
+        }else{
+            $('#result').text("Eat at home");
+        }
     });
 });
