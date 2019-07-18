dd726d7f8e74eaa8e07cb270db0845dd39a36992
Kiet Huynh <dragonunc11@gmail.com>
2019-07-17 22:42:38 -0400

    Format code w/ Prettier

--- js/restaurant-selector.js
+++ js/restaurant-selector.js
@@ -1,21 +1,21 @@
 var restaurants = [
-  {name: "Cook Out", coords:{lat:35.140730,lng:-80.735529}},
-  {name: "Kentucky Fried Chicken", coords:{lat:35.193522,lng:-80.795222}},
-  {name: "McDonalds", coords:{lat:35.183293, lng:-80.759337}},
-  {name: "Costco", coords:{lat:35.130173, lng: -80.704982}},
-  {name: "21 South Drive In", coords: {lat:35.203351, lng:-80.790616}},
-  {name: "Seoul Food", coords: {lat:35.219263, lng:-80.857579}},
-  {name: "Midnight Diner", coords: {lat:35.219558, lng:-80.852221}},
-  {name: "JJ Red Hots", coords: {lat:35.199979, lng:-80.842400}},
-  {name: "Taipei Express", coords: {lat:35.199843, lng:-80.825259}},
-  {name: "Cowbell", coords: {lat:35.228537, lng:-80.842344}},
-  {name: "Mr. Tokyo", coords: {lat:35.126808, lng:-80.708284}},
-  {name: "Taco Bell", coords: {lat:35.140805, lng:-80.733992}},
-  {name: "Soho Bistro", coords: {lat:35.227608, lng:-80.840376}}
+  { name: "Cook Out", coords: { lat: 35.14073, lng: -80.735529 } },
+  { name: "Kentucky Fried Chicken", coords: { lat: 35.193522, lng: -80.795222 } },
+  { name: "McDonalds", coords: { lat: 35.183293, lng: -80.759337 } },
+  { name: "Costco", coords: { lat: 35.130173, lng: -80.704982 } },
+  { name: "21 South Drive In", coords: { lat: 35.203351, lng: -80.790616 } },
+  { name: "Seoul Food", coords: { lat: 35.219263, lng: -80.857579 } },
+  { name: "Midnight Diner", coords: { lat: 35.219558, lng: -80.852221 } },
+  { name: "JJ Red Hots", coords: { lat: 35.199979, lng: -80.8424 } },
+  { name: "Taipei Express", coords: { lat: 35.199843, lng: -80.825259 } },
+  { name: "Cowbell", coords: { lat: 35.228537, lng: -80.842344 } },
+  { name: "Mr. Tokyo", coords: { lat: 35.126808, lng: -80.708284 } },
+  { name: "Taco Bell", coords: { lat: 35.140805, lng: -80.733992 } },
+  { name: "Soho Bistro", coords: { lat: 35.227608, lng: -80.840376 } }
 ];
 
 var map, marker, infowindow;
-var markers=[];
+var markers = [];
 
 function getRestaurant() {
   return restaurants[Math.floor(Math.random() * restaurants.length)];
@@ -24,28 +24,28 @@ function getRestaurant() {
 function initMap() {
   //Map options
   var options = {
-    zoom:11,
-    center: {lat: 35.2271, lng: -80.8431 }
-  }
+    zoom: 11,
+    center: { lat: 35.2271, lng: -80.8431 }
+  };
   //New Map
-  map = new google.maps.Map(document.getElementById('map'), options);
-  
-  for (restaurant of restaurants){
+  map = new google.maps.Map(document.getElementById("map"), options);
+
+  for (restaurant of restaurants) {
     markers[restaurant.name] = new google.maps.Marker({
       position: restaurant.coords,
       map: map,
       title: restaurant.name,
       animation: google.maps.Animation.BOUNCE,
-      visible: false,
-    })
+      visible: false
+    });
   }
   console.log(markers);
   console.log(markers.length);
 }
 
-function removeMarker(){
-  for (restaurant of restaurants){
-    if (markers[restaurant.name].getVisible() == true){
+function removeMarker() {
+  for (restaurant of restaurants) {
+    if (markers[restaurant.name].getVisible() == true) {
       markers[restaurant.name].setVisible(false);
       console.log("removed");
     }
@@ -64,7 +64,7 @@ $(document).ready(function() {
 
   $(".roll1").click(function() {
     removeMarker();
-    restaurant = getRestaurant()
+    restaurant = getRestaurant();
     var result = restaurant.name;
     markers[restaurant.name].setVisible(true);
     $("#result").text(result);
@@ -81,7 +81,9 @@ $(document).ready(function() {
         markers[restaurant.name].setVisible(true);
       }
     }
-    $("#result").text(result[0].name + ", " + result[1].name + ", " + result[2].name);
+    $("#result").text(
+      result[0].name + ", " + result[1].name + ", " + result[2].name
+    );
   });
 
   $(".eat-out").click(function() {
